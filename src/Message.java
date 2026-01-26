66d6e5e222e65c34bcca3541f9622684ac007454
Rajdeep <rajdeepxchintan@gmail.com>
2026-01-27 01:47:45 +0530

    Additional Changes

--- src/Message.java
+++ src/Message.java
@@ -1,7 +1,15 @@
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
 public class Message implements Serializable {
     public RequestType type;
     public String fileName;
+    public List<String> chunkList;
+    // Used by ChunkServer for READ/WRITE operations
     public byte[] data;
+
+    // Response fields
+    public List<String> chunks;
+    public Map<String, List<String>> chunkLocations;
 }
